6fbe8c12fb067a6e5406d6a1459676ea5b21249e
Ntrash748 <919120347@qq.com>
2021-06-20 12:53:30 +0800

    封装featureview

---
+++ src/views/home/childComponents/FeatureView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='feature'>
+    <a href="">
+      <img src="~assets/img/featureview/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureView',
+  data(){
+    return {}
+  },
+  components: {},
+  created(){},
+  mounted(){},
+  methods: {}
+}
+</script>
+<style scoped>
+.feature img {
+  width: 100%;
+}
+</style>
